8d1138075ee637cf01ad1eda7527f6a6de88fa1f
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-07-09 23:04:31 +0200

    refactor: reformat App.tsx

--- betterbet/src/App.tsx
+++ betterbet/src/App.tsx
@@ -1,33 +1,35 @@
 import React from 'react'
 import { Box, CssBaseline } from "@mui/material"
 import { routes } from "./routes"
-import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
-  return (
-      <div>
-        <CssBaseline />
-        <Box
-            height="100vh"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            flexDirection="column"
-        >
-            <Router>
-                <Routes>
-                    {routes.map((route) => (
-                        <Route
-                            key={route.key}
-                            path={route.path}
-                            element={<route.component />}
-                        />
-                    ))}
-                </Routes>
-            </Router>
-        </Box>
-      </div>
-  );
+    return (
+        <div>
+            <CssBaseline/>
+            <Box
+                height="100vh"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+            >
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        {routes.map((route) => (
+                            <Route
+                                key={route.key}
+                                path={route.path}
+                                element={<route.component/>}
+                            />
+                        ))}
+                    </Routes>
+                </Router>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
