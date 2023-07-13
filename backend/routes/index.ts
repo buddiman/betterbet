11c3106559ed93b8c5946c949a578226c12f7428
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-07-13 23:41:56 +0200

    feat: add authentication route

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -3,6 +3,7 @@ import { defaultRoute } from './defaultRoute'
 import { betRoute } from "./betRoute";
 import { leagueRoute } from "./leagueRoute";
 import { eventRoute } from "./eventRoute";
+import { authRoute } from "./authRoute";
 
 export const routes = express.Router()
 
@@ -10,3 +11,4 @@ routes.use(defaultRoute)
 routes.use(betRoute)
 routes.use(leagueRoute)
 routes.use(eventRoute)
+routes.use(authRoute)
--- betterbet/src/routes.ts
+++ betterbet/src/routes.ts
@@ -1,15 +1,17 @@
 import Home from "./pages/Home";
 import Bets from "./pages/Bets";
 import Login from "./pages/Login";
+import Manage from "./pages/Manage";
 
 import {FC} from "react";
+import Register from "./pages/Register";
 
 interface Route {
     key: string,
     title: string,
     path: string,
     enabled: boolean,
-    component: FC<{}>
+    component: FC<{}> | FC<any>
 }
 
 export const routes: Array<Route> = [
@@ -33,5 +35,19 @@ export const routes: Array<Route> = [
         path: '/login',
         enabled: true,
         component: Login
+    },
+    {
+        key: 'register-route',
+        title: 'Register',
+        path: '/register',
+        enabled: true,
+        component: Register
+    },
+    {
+        key: 'manage-route',
+        title: 'Manage',
+        path: '/manage',
+        enabled: true,
+        component: Manage
     }
 ]
