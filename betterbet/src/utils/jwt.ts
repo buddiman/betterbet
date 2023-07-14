c361b33f4d7afd7549753678b4cd03b500db6dfb
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-07-14 11:20:02 +0200

    refactor: remove unused files

--- betterbet/src/utils/jwt.ts
+++
@@ -1,6 +0,0 @@
-import jwt_decode from 'jwt-decode';
-
-export function getUserIdFromJwtToken(token: string) {
-    const decodedToken: { userId: string } = jwt_decode(token);
-    return parseInt(decodedToken.userId)
-}
