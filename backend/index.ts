712d5145fb3ca218315544e11116154498cc6402
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-07-14 20:55:00 +0200

    refactor: remove unused imports from backend startup

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,8 @@
-import { Bet } from "../shared/models/bet"
 import { routes } from './routes'
 
 import express, { Express } from "express";
 import cors from "cors";
 
-import { createBet, getBet } from "./database/dto/bet";
-import { createEvent } from "./database/dto/event";
-import { createSportType } from "./database/dto/sportType";
-import { createLeague } from "./database/dto/league";
-import { scrape } from "./scraper/scraper";
 
 const app: Express = express();
 
