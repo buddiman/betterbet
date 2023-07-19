55b50c1a0cd9c45e6fb9cf7e76a9aff8a855208f
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-07-19 23:21:08 +0200

    feat(frontend): basic functionality for statistics page

--- betterbet/src/pages/Statistics.tsx
+++ betterbet/src/pages/Statistics.tsx
@@ -1,20 +1,63 @@
 import React, { ReactElement, FC, useEffect } from "react";
-import { Box, Button, ButtonGroup, Grid, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Grid, IconButton,
+    MenuItem,
+    Paper,
+    Select,
+    SelectChangeEvent, TableBody, TableCell, TableContainer,
+    TableHead,
+    TableRow, Tooltip
+} from "@mui/material";
 import { Event } from "shared/models/event";
 import { Bet } from "shared/models/bet";
 import api from "../api";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { BetInstance } from "shared/models/betInstance";
 
+interface UsernameWithId {
+    id: number
+    username: string
+}
+
+interface BetWithInstances extends Bet {
+    BetInstance: BetInstance[]
+}
 
 const Statistics: FC = (): ReactElement => {
     const [eventList, setEventList] = React.useState<Event[] | undefined>([])
+    const [userList, setUserList] = React.useState<UsernameWithId[] | undefined>([])
     const [eventIdValue, setEventIdValue] = React.useState<any>(null)
-    const [displayedBets, setDisplayedBets] = React.useState<Bet[] | undefined>(undefined)
+    const [displayedBets, setDisplayedBets] = React.useState<BetWithInstances[] | undefined>(undefined)
+
+    const backgroundColor = (points: number|undefined) => {
+        if(points === undefined) {
+            return 'red'
+        } else {
+            if (points < 0) {
+                return 'grey';
+            } else if (points === 0) {
+                return 'red';
+            } else if (points === 1) {
+                return 'yellow';
+            } else if (points === 2) {
+                return 'green';
+            } else if (points === 3) {
+                return 'purple';
+            }
+        }
+    }
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await api.get("/events")
                 setEventList(response.data.event);
+                const usersResponse = await api.get("/users")
+                setUserList(usersResponse.data.users)
             } catch (error) {
                 console.error('Error fetching data from API:', error);
             }
@@ -25,29 +68,31 @@ const Statistics: FC = (): ReactElement => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await api.get(`/bets/${eventIdValue}`)
-                const bets: Bet[] = response.data.bets
-                setDisplayedBets(bets.filter(bet => bet.date < new Date()))
+                const response = await api.get(`/betsWithInstances/${eventIdValue}`)
+                const bets: BetWithInstances[] = response.data.bets
+                setDisplayedBets(bets.filter(bet => new Date(bet.date) < new Date()))
             } catch (error) {
                 console.error('Error fetching data from API:', error);
             }
         };
         fetchData();
+
     }, [eventIdValue])
+
+    const getBetInstance = async (userId: number, betId: number) => {
+        const response = await api.get(`/betInstance/${userId}/${betId}`)
+        if (response.data.betInstance) {
+            return response.data.betInstance.userBet
+        } else {
+            return "---"
+        }
+    }
 
     const handleSelectEventChange = async (event: SelectChangeEvent<any>) => {
         if (eventList) {
             const selectedEventId = event.target.value as string;
             const selectedEvent = eventList.find(event => event.id === parseInt(selectedEventId));
             setEventIdValue(selectedEvent?.id || null);
-            if (selectedEvent) {
-                try {
-                    const response = await api.get(`/bets/${selectedEvent.id}`)
-                    //setManagedBets(response.data.bets)
-                } catch (e) {
-                    console.log(e)
-                }
-            }
         }
     }
 
@@ -77,7 +122,49 @@ const Statistics: FC = (): ReactElement => {
                     </Select>
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                    {eventIdValue}
+                    <TableContainer component={Paper}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">Team 1</TableCell>
+                                <TableCell align="center">Team 2</TableCell>
+                                <TableCell align="center">Typ</TableCell>
+                                <TableCell align="center">Kondition</TableCell>
+                                <TableCell align="center">Frage</TableCell>
+                                {userList && userList.map((e) => (
+                                    <TableCell align="center">{e.username}</TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {displayedBets && displayedBets.map((e) => (
+                                <TableRow
+                                    key={e.id}
+                                >
+                                    <TableCell align="center">{e.teamHomeDescription}</TableCell>
+                                    <TableCell align="center">{e.teamAwayDescription}</TableCell>
+                                    <TableCell align="center">{e.type}</TableCell>
+                                    <TableCell align="center">{e.typeCondition}</TableCell>
+                                    <TableCell align="center">
+                                        {e.question && (
+                                            <Tooltip title={e.question}>
+                                                <IconButton>
+                                                    <QuestionMarkIcon/>
+                                                </IconButton>
+                                            </Tooltip>
+                                        )}
+                                    </TableCell>
+                                    {userList && userList.map((u) => (
+                                        <TableCell
+                                            align="center"
+                                            sx={{ backgroundColor:backgroundColor((e.BetInstance.find((b) => b.userId === u.id)?.points))}}
+                                        >
+                                            {(e.BetInstance.find((b) => b.userId === u.id))?.userBet || "---"}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </TableContainer>
                 </Grid>
             </Grid>
 
