daeae3c465ff8b89854ceea3a5ba4eadc2dad1cf
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-08-09 21:02:07 +0200

    feat(frontend): append the number of open bets to the select box entries

--- betterbet/src/pages/Bets.tsx
+++ betterbet/src/pages/Bets.tsx
@@ -3,10 +3,17 @@ import { Box, Select, MenuItem, Stack, SelectChangeEvent, InputLabel } from "@mu
 import BetWidget from '../components/BetWidget'
 import { Event } from "shared/models/event"
 import api from "../api"
+import * as AuthService from "../services/auth.service";
 
+interface MissingBetEvent {
+    eventId: number
+    name: string
+    count: number
+}
 const Bets: FC<any> = (): ReactElement => {
     const [events, setEvents] = useState<Event[]>([]);
-    const [selectedEvent, setSelectedEvent] = useState<number>(1); // Initialize with an empty string
+    const [selectedEvent, setSelectedEvent] = useState<number>(1);
+    const [missingBetEvents, setMissingBetEvents] = useState<MissingBetEvent[] | undefined>(undefined)
 
     const handleSelectedEventChange = (event: SelectChangeEvent<any>) => {
         const eventId = event.target.value as number;
@@ -17,14 +24,28 @@ const Bets: FC<any> = (): ReactElement => {
     useEffect(() => {
         const fetchData = async () => {
             try {
+                const user = AuthService.getCurrentUser()
                 const response = await api.get("/events");
                 setEvents(response.data.event);
+                const eventsWithMissingBets = await api.post("/missingBetEvents", {
+                    userId: user.id
+                })
+                setMissingBetEvents(eventsWithMissingBets.data.events)
             } catch (error) {
                 console.error('Error fetching data from API:', error);
             }
         };
         fetchData();
     }, []);
+
+    function getMissingNumberString(id: number): string {
+        const missingNumberEvent = missingBetEvents?.find(e => e.eventId === id)
+        if(missingNumberEvent) {
+            return " (" + missingNumberEvent.count + " offen)"
+        } else {
+            return ""
+        }
+    }
 
     return (
         <Box sx={{
@@ -44,7 +65,7 @@ const Bets: FC<any> = (): ReactElement => {
                 >
                     {events.map((e) => (
                         <MenuItem key={e.id} value={e.id}>
-                            {e.name}
+                            {e.name}{getMissingNumberString(e.id)}
                         </MenuItem>
                     ))}
                 </Select>
