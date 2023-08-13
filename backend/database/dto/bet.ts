9d7e9dccac6684b011c04ab2a700c9f2a2f4c407
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2023-08-13 12:32:58 +0200

    feat(backend): add route for user points per event

--- backend/database/dto/bet.ts
+++ backend/database/dto/bet.ts
@@ -1,6 +1,12 @@
 import { Bet } from "shared/models/bet"
 import prisma from "../dbConfig";
 
+interface UserPoints {
+    userId: number;
+    points: number;
+    hasMostPoints: boolean;
+}
+
 export async function createBet(bet: Bet): Promise<Bet> {
     return prisma.bet.create({
         data: bet
@@ -85,4 +91,57 @@ export function getNotEvaluatedBetsInThePast(): Promise<Bet[]> {
             }
         }
     })
+}
+
+export async function getPlayerPointsPerEvent() {
+    const users = await prisma.user.findMany();
+    const events = await prisma.event.findMany();
+
+    const userEventPoints = [];
+
+    for (const event of events) {
+        const betInstances = await prisma.betInstance.findMany({
+            where: {
+                bet: { eventId: event.id },
+                points: {
+                    not: -1 // Ignore instances with points = -1
+                }
+            },
+            include: { user: true },
+        });
+
+        const pointsPerUser: Record<number, UserPoints> = {};
+
+        // Initialize pointsPerUser with 0 points for all users
+        for (const user of users) {
+            pointsPerUser[user.id] = {
+                userId: user.id,
+                points: 0,
+                hasMostPoints: false,
+            };
+        }
+
+        for (const betInstance of betInstances) {
+            const user = betInstance.user;
+            const points = betInstance.points || 0;
+
+            pointsPerUser[user.id].points += points;
+        }
+
+        userEventPoints.push({
+            eventId: event.id,
+            eventName: event.name,
+            pointsPerUser: Object.values(pointsPerUser),
+        });
+    }
+
+    return userEventPoints.map(eventData => {
+        const highestPoints = Math.max(...eventData.pointsPerUser.map(user => user.points));
+
+        eventData.pointsPerUser.forEach(user => {
+            user.hasMostPoints = user.points === highestPoints;
+        });
+
+        return eventData;
+    });
 }
